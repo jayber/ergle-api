660cf81e74e12083d87898853ea4cd93374132f8
James <james.a.bromley@googlemail.com>
2014-09-10 23:22:18 +0100

    synch timelines

--- app/models/Event.scala
+++ app/models/Event.scala
@@ -7,12 +7,7 @@ object Event {
   def categoriseDate(sortDate: Date): String = {
     val dateFormat: SimpleDateFormat = new SimpleDateFormat("d MMM ''yy")
 
-    val instance: Calendar = Calendar.getInstance
-    instance.setTime(sortDate)
-    instance.set(Calendar.HOUR_OF_DAY, 0)
-    instance.set(Calendar.MINUTE, 0)
-    instance.set(Calendar.SECOND, 0)
-    instance.set(Calendar.MILLISECOND, 0)
+    val instance: Calendar = categoryDate(sortDate)
 
     val today: Calendar = Calendar.getInstance
     today.set(Calendar.HOUR_OF_DAY, 0)
@@ -35,11 +30,25 @@ object Event {
       case _ => dateFormat.format(instance.getTime)
     }
   }
+
+  def categoryDate(sortDate: Date): Calendar = {
+    val instance: Calendar = Calendar.getInstance
+    instance.setTime(sortDate)
+    instance.set(Calendar.HOUR_OF_DAY, 0)
+    instance.set(Calendar.MINUTE, 0)
+    instance.set(Calendar.SECOND, 0)
+    instance.set(Calendar.MILLISECOND, 0)
+    instance
+  }
 }
 
 case class Event(id:String, owner: String, eventType: String, title: Option[String], sortDate: Date, link: Option[String], tag: Option[String], to: Seq[String], content: Option[String], comments: Option[Array[Comment]]) {
   def dateCategory = {
     Event.categoriseDate(sortDate)
+  }
+
+  def categoryDate = {
+    Event.categoryDate(sortDate)
   }
 
   def withOthers(excludeEmail: String*) = {
